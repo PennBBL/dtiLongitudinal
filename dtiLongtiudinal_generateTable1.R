7962099b4b426a3d577c0384b632a7904f79de2a
Angel Garcia de la Garza <agarza@compute-0-0.local>
2017-04-20 12:21:43 -0400

    Updated Table1 Script

--- dtiLongtiudinal_generateTable1.R
+++ dtiLongtiudinal_generateTable1.R
@@ -12,7 +12,7 @@ demo$race2[which(demo$race2 == 3)] <- 2
 demo$race2 <- as.factor(demo$race2)
 demo$handedness <- as.factor(demo$handedness)
 demo$AgeAtScan <- demo$AgeAtScan / 12
-
+demo$dx_t1_t2_clean <- as.factor(demo$dx_t1_t2_clean)
 
 
 
@@ -57,3 +57,60 @@ tableOne2Sup <- CreateTableOne(vars = vars, strata = c("dx_expanded_psyispro_t1_
 t2Sup <- print(tableOne2Sup, argsApprox = c("sex","race2"), test=T, noSpaces=T)
 write.csv(t2Sup, "/data/joy/BBL/projects/dtilongitudinal/output/dtiLongitudinal_supplementtable1_timepoint2.csv")
 
+
+##Calculate Statistics 
+
+contvars <- c("AgeAtScan","meduCnb",
+          "positive","negative",
+          "disorganized","general",
+          "sevSipsMean","SumSips",
+          "feduCnb","gaf_z","gaf_raw","dti64MeanRelRMS","dti64Tsnr")
+catvars <- c("race2","sex","handedness")
+
+group = "dx_t1_t2_clean"
+
+data <- demo1
+
+test.statistics <- function(catvars, contvars, group, data) {
+  
+  data[,which(names(data) == group)] <- as.factor(as.character(data[,which(names(data) == group)]))
+  
+  for (i in catvars) {
+    if (all(is.na(data[which(names(data) == i)]))) {
+      catvars <- catvars[-which(catvars == i )]
+    }
+  }
+    
+  for (i in contvars) {
+    if (all(is.na(data[which(names(data) == i)]))) {
+      contvars <- contvars[-which(contvars == i )]
+    }
+  }
+  
+  catStat <- t(mcmapply(function(x) {
+    t <- chisq.test(data[, which(names(data) == x)],data[, which(names(data) == group)])
+    c(x,t$p.value, t$statistic, t$parameter)
+  }, catvars, SIMPLIFY=T))
+
+  
+  if (length(levels(data[,which(names(data) == group)])) > 2) {
+    contStat <- t(mcmapply(function(x) {
+      formula <- as.formula(paste(x, "~", group))
+      t <- oneway.test(formula, data, var.equal=F)
+      c(x,t$p.value, t$statistic, t$parameter)
+    }, contvars, SIMPLIFY=T))
+  } else if (length(levels(data[,which(names(data) == group)])) == 2) {
+    contStat <- t(mcmapply(function(x) {
+      formula <- as.formula(paste(x, "~", group))
+      t <- t.test(formula, data, var.equal=F)
+      c(x,t$p.value, t$statistic, t$parameter)
+    }, contvars, SIMPLIFY=T))
+  }
+  
+  row.names(contStat) <- NULL
+  row.names(catStat) <- NULL
+  
+  return(list(catStat, contStat))
+}
+
+temp <- test.statistics(catvars, contvars, group, data = demo2)
